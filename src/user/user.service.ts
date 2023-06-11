73a6f84ba441707fdcebe35af3621847f2c9ad40
mario andre <mario.andre.franco@gmail.com>
2023-06-11 11:36:04 -0300

    start validation and compare passwords

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -21,7 +21,7 @@ export class UserService {
   }
 
   findByEmail(email: string) {
-    this.prisma.user.findUnique({
+    return this.prisma.user.findUnique({
       where: {
         email,
       },
